33a77461c12e1ffcbbb301a5314a21b0362012ec
TheInvisibility <45173598+TheInvisibility@users.noreply.github.com>
2021-04-23 19:32:16 +0200

    Initial commit

---
+++ config.h
@@ -0,0 +1,20 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/*Enable blur*/
+
+#define BLUR
+static const int blurRadius=5;
+
+#define PIXELATION
+static const int pixelSize=10;
